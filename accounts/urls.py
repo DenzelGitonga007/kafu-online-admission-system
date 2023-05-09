ea824f2727b2f5db13d170e218a544df9e594235
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-09 11:51:35 +0300

    add the register link to the UI

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.home, name='home'), # home view/ landing page
     path('register/', views.register_user, name='register'), #registration form
