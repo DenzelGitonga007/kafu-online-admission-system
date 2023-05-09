7e66fea4ba2edc05ce506b09501877d7c2ae7850
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-09 10:49:03 +0300

    trying to display the fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+# from django.contrib import admin
+# # Customize the list display of the default django user on the admin site
+# from django.contrib.auth.admin import UserAdmin
+# # Import the model
+# from . models import User
 
-# Register your models here.
+# class CustomUserAdmin(UserAdmin):
+#     list_display = ('username', 'email', 'is_staff', 'is_superuser')
+
+# admin.site.register(User, CustomUserAdmin)
