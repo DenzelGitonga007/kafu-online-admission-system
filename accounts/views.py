b435b62c95330d838a73643ec514e8d704e43d62
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-09 11:47:20 +0300

    views for register

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, request
+from  .forms import UserRegistrationForm
 
 # Create your views here.
 
 # Home view
 def home(request):
     return render(request, 'accounts/home.html', context={})
+
+
+# Creating the user
+def register_user(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('home') # the home url pattern
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
