42e13a8a62a290427238e016a8c91089ee4c2615
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-09 13:05:07 +0300

    messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from  .forms import UserRegistrationForm
+# To display message
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,8 +16,12 @@ def register_user(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            # Success message
+            messages.success(request, "Congratulations! You have created an account successfully")
             return redirect('home') # the home url pattern
+        # If error in creation
+        else:
+            messages.error(request, 'There was an error while creating your account. Please try again.')
     else:
-        
         form = UserRegistrationForm()
     return render(request, 'accounts/register.html', {'form': form})
