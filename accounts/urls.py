596ef60ac637528cbd84ba4b92630a18a9b0389f
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-09 11:48:35 +0300

    registration url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'), # home view/ landing page
+    path('register/', views.register_user, name='register'), #registration form
 ]
