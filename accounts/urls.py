eb09c56ba091c458829e2d74fda62f45c682a91b
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-10 09:43:13 +0300

    login url

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), # home view/ landing page
     path('register/', views.register_user, name='register'), #registration form
+    path('login/', views.login_user, name='login'), #login page
 ]
