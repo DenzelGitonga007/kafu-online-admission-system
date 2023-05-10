1947b23b6f5d735de503275c8311405167f37dc3
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-10 11:47:53 +0300

    login validation errors

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from . models import User
-
+# Authentication
+from django.contrib.auth import authenticate
 class UserRegistrationForm(UserCreationForm):
     # username comes by default
     email = forms.EmailField(required=True)
@@ -25,3 +26,19 @@ class UserRegistrationForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+        # if username and password:
+        #     user = authenticate(username=username, password=password)
+        #     if not user:
+        #         raise forms.ValidationError("Invalid username or password")
+        # else:
+        #     raise forms.ValidationError("Please enter both username and password")
+        user = authenticate(username=username, password=password)
+        if not user:
+            self.add_error('', 'Invalid username or password')
+            raise forms.ValidationError('Please check the credentials you provided')
+        return cleaned_data
--- accounts/views.py
+++ accounts/views.py
@@ -42,8 +42,9 @@ def login_user(request):
                 messages.success(request, "Congratulations! You have logged in successfully!")
                 # Redirect them
                 return redirect('home')
-            else:
-                form.add_error(None, 'Invalid username or password')
+            # else:
+            #     # Error message if credentials are wrong
+            #     form.add_error(None, 'Invalid username or password')
     else:
         form = LoginForm()
     return render(request, 'accounts/login.html', {'form': form})
