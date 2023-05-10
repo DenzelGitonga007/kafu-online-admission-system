6c04273270b8f58cd381d82a769d2a8c26f4bb07
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-10 10:05:17 +0300

    logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'), # home view/ landing page
     path('register/', views.register_user, name='register'), #registration form
     path('login/', views.login_user, name='login'), #login page
+    path('logout/', views.logout_user, name='logout'), #logout page
 ]
