b436eec4f2ba261a9ad5475e1b5abd80943f27aa
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-12 11:05:16 +0300

    parent detail url

--- admissions/urls.py
+++ admissions/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'admissions'
 
 urlpatterns = [
-    path('personal-details/', views.personal_detail_view, name='personal_details'),
+    path('personal-details/', views.personal_detail_view, name='personal_details'), # personal details
+    path('parent-details/', views.parent_detail_view, name='parent_details'), # parent details
 ]
