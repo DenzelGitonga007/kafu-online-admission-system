32a880e2166a9dc6a123eacace86d689a11cd1bf
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-12 10:55:06 +0300

    parent detail admin

--- admissions/admin.py
+++ admissions/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import PersonalDetail
+from .models import PersonalDetail, ParentDetail
 
 # To customize the Student display
+# Get the username for all details
+def username(obj):
+    return obj.user.username
+
+# Personal details
 class PersonalDetailAdmin(admin.ModelAdmin):
-    list_display = ('username','surname', 'first_name', 'last_name', 'email', 'phone', 'city')
+    list_display = (username,'surname', 'first_name', 'last_name', 'email', 'phone', 'city')
 
-    def username(self, obj):
-        return obj.user.username
 
-# admin.site.register(Student, StudentAdmin)
-admin.site.register(PersonalDetail, PersonalDetailAdmin)
+
+# Parent details
+class ParentDetailAdmin(admin.ModelAdmin):
+    list_display = (username, 'father_first_name', 'mother_first_name', 'guardian_first_name')
+
+admin.site.register(PersonalDetail, PersonalDetailAdmin) # Personal details
+admin.site.register(ParentDetail, ParentDetailAdmin) # Parent details
