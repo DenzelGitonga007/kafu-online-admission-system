d0822ae15833923bb2435a6029d78d78729b4370
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-17 10:05:47 +0300

    attempt to download

--- forms_downloads/urls.py
+++ forms_downloads/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from . import views
 app_name = 'forms_downloads'
 
 urlpatterns = [
-
+    path('download_personal_details/<int:user_id>/', views.download_personal_details, name='download_personal_details'),
 ]
--- forms_downloads/views.py
+++ forms_downloads/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404
+from django.conf import settings
+import os
+
+from admissions.models import PersonalDetail
+
+from django.template.loader import get_template
+
+def download_personal_details(request, user_id):
+    # Retrieve the personal details form for the user
+    personal_details = PersonalDetail.objects.get(user_id=user_id)
+
+    # Generate a download file using a template
+    template = get_template('forms_downloads/personal_details_template.txt')
+    download_file = template.render({'personal_details': personal_details})
+
+    # Create an HTTP response with the download file
+    response = HttpResponse(download_file, content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename="personal_details.txt"'
+    return response
+
