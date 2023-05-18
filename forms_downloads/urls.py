ca11b326c7dfe7800b7e35d76497afd420b2a616
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-18 09:29:08 +0300

    download spouse details

--- forms_downloads/urls.py
+++ forms_downloads/urls.py
@@ -5,4 +5,5 @@ app_name = 'forms_downloads'
 urlpatterns = [
     path('download_personal_details/<int:user_id>/', views.download_personal_details, name='download_personal_details'), # personal details download
     path('download_parent_details/<int:user_id>/', views.download_parent_details, name='download_parent_details'), # personal details download
+    path('download_spouse_details/<int:user_id>/', views.download_spouse_details, name='download_spouse_details'), # personal details download
 ]
--- forms_downloads/views.py
+++ forms_downloads/views.py
@@ -6,7 +6,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from io import BytesIO
 
-from admissions.models import PersonalDetail, ParentDetail
+from admissions.models import PersonalDetail, ParentDetail, SpouseDetail
 
 # Download personal details
 def download_personal_details(request, user_id):
@@ -103,6 +103,7 @@ def download_personal_details(request, user_id):
 
     return response
 
+# End of personal details
 
 # Download parent details
 def download_parent_details(request, user_id):
@@ -214,6 +215,92 @@ def download_parent_details(request, user_id):
     # Set the response headers for the PDF file
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="parent_details.pdf"'
+
+    # Write the buffer's content to the response
+    response.write(buffer.getvalue())
+
+    return response
+
+# End of personal details
+
+# Download spouse details
+def download_spouse_details(request, user_id):
+    spouse_details = SpouseDetail.objects.get(user_id=user_id)
+
+    # Create a BytesIO buffer to receive the PDF data
+    buffer = BytesIO()
+
+    # Create the PDF object, using the buffer as its "file"
+    p = canvas.Canvas(buffer, pagesize=letter)
+
+    # Set the font and font size for the PDF
+    p.setFont("Helvetica", 12)
+
+    # Draw the school logo in the header
+    logo_path = 'static/images/logos/KAFU_LOGO.jpg'  # Replace with the actual path to the school logo
+    logo_width = 100
+    logo_height = 100
+    logo_x = (letter[0] - logo_width) / 2  # Center horizontally
+    logo_y = 650  # Adjust the vertical position as desired
+    p.drawImage(logo_path, logo_x, logo_y, width=logo_width, height=logo_height)
+
+    # Set the header text
+    text_y = 635
+    p.setFont("Helvetica-Bold", 16)
+    p.drawString(200, text_y, "KAIMOSI FRIENDS UNIVERSITY")
+    p.setFont("Helvetica-Bold", 12)
+    p.drawString(270, text_y-20, "Online Admission")
+
+    # The student
+    text_y = 570  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Username: {}".format(spouse_details.user.username))
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "Spouse Details Form")
+
+
+    # Write the personal details to the PDF
+    # Spouse
+    # Name section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Spouse")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "Surname: {}              First Name: {}          Last Name: {}".format(spouse_details.spouse_surname, spouse_details.spouse_first_name, spouse_details.spouse_last_name))
+
+    # Nationality section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Nationality")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "National ID: {}              Occupation: {}".format(spouse_details.spouse_national_id, spouse_details.spouse_occupation))
+    
+    # Address section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Address")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "Email: {}                Phone: {}".format(spouse_details.spouse_email, spouse_details.spouse_phone))
+
+    # Location section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Location")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "City: {}             P.O BOX: {}".format(spouse_details.spouse_city, spouse_details.spouse_pob))
+
+    # End of Spouse
+
+    # Save the PDF to the buffer
+    p.showPage()
+    p.save()
+
+    # Set the buffer's position back to the beginning
+    buffer.seek(0)
+
+    # Set the response headers for the PDF file
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="spouse_details.pdf"'
 
     # Write the buffer's content to the response
     response.write(buffer.getvalue())
