4e99e79a40471c66308aa8a769aeb7569fb3928a
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-18 09:11:33 +0300

    download parent details

--- forms_downloads/urls.py
+++ forms_downloads/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'forms_downloads'
 
 urlpatterns = [
-    path('download_personal_details/<int:user_id>/', views.download_personal_details, name='download_personal_details'),
+    path('download_personal_details/<int:user_id>/', views.download_personal_details, name='download_personal_details'), # personal details download
+    path('download_parent_details/<int:user_id>/', views.download_parent_details, name='download_parent_details'), # personal details download
 ]
--- forms_downloads/views.py
+++ forms_downloads/views.py
@@ -6,8 +6,9 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from io import BytesIO
 
-from admissions.models import PersonalDetail
+from admissions.models import PersonalDetail, ParentDetail
 
+# Download personal details
 def download_personal_details(request, user_id):
     personal_details = PersonalDetail.objects.get(user_id=user_id)
 
@@ -38,9 +39,9 @@ def download_personal_details(request, user_id):
     # The student
     text_y = 570  # Adjust the vertical position as desired
     p.setFont("Helvetica-Bold", 14)
-    p.drawString(50, text_y, "Student")
+    p.drawString(50, text_y, "Username: {}".format(personal_details.user.username))
     p.setFont("Helvetica", 12)
-    p.drawString(50, text_y - 20, "Username: {}".format(personal_details.user.username))
+    p.drawString(50, text_y - 20, "Personal Details Form")
 
 
     # Write the personal details to the PDF
@@ -96,6 +97,123 @@ def download_personal_details(request, user_id):
     # Set the response headers for the PDF file
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="personal_details.pdf"'
+
+    # Write the buffer's content to the response
+    response.write(buffer.getvalue())
+
+    return response
+
+
+# Download parent details
+def download_parent_details(request, user_id):
+    parent_details = ParentDetail.objects.get(user_id=user_id)
+
+    # Create a BytesIO buffer to receive the PDF data
+    buffer = BytesIO()
+
+    # Create the PDF object, using the buffer as its "file"
+    p = canvas.Canvas(buffer, pagesize=letter)
+
+    # Set the font and font size for the PDF
+    p.setFont("Helvetica", 12)
+
+    # Draw the school logo in the header
+    logo_path = 'static/images/logos/KAFU_LOGO.jpg'  # Replace with the actual path to the school logo
+    logo_width = 100
+    logo_height = 100
+    logo_x = (letter[0] - logo_width) / 2  # Center horizontally
+    logo_y = 650  # Adjust the vertical position as desired
+    p.drawImage(logo_path, logo_x, logo_y, width=logo_width, height=logo_height)
+
+    # Set the header text
+    text_y = 635
+    p.setFont("Helvetica-Bold", 16)
+    p.drawString(200, text_y, "KAIMOSI FRIENDS UNIVERSITY")
+    p.setFont("Helvetica-Bold", 12)
+    p.drawString(270, text_y-20, "Online Admission")
+
+    # The student
+    text_y = 570  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Username: {}".format(parent_details.user.username))
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "Parent Details Form")
+
+
+    # Write the personal details to the PDF
+    # Father
+    # Name section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Father")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "Surname: {}              First Name: {}          Last Name: {}".format(parent_details.father_surname, parent_details.father_first_name, parent_details.father_last_name))
+
+    # Nationality section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Nationality")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "National ID: {}              Occupation: {}".format(parent_details.father_national_id, parent_details.father_occupation))
+    # End of Father
+
+    # Mother
+    # Name section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Mother")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "Surname: {}              First Name: {}          Last Name: {}".format(parent_details.mother_surname, parent_details.mother_first_name, parent_details.mother_last_name))
+
+    # Nationality section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Nationality")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "National ID: {}              Occupation: {}".format(parent_details.mother_national_id, parent_details.mother_occupation))
+    # End of Mother
+
+    # Guardian
+    # Name section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Guardian")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "Surname: {}              First Name: {}          Initial Name: {}".format(parent_details.guardian_surname, parent_details.guardian_first_name, parent_details.guardian_initial_name))
+
+    # Nationality section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Nationality")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "National ID: {}              Occupation: {}".format(parent_details.guardian_national_id, parent_details.guardian_occupation))
+
+    # Address section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Address")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "Email: {}                Phone: {}".format(parent_details.guardian_email, parent_details.guardian_phone))
+
+    # Birth section heading
+    text_y -= 70  # Adjust the vertical position as desired
+    p.setFont("Helvetica-Bold", 14)
+    p.drawString(50, text_y, "Location")
+    p.setFont("Helvetica", 12)
+    p.drawString(50, text_y - 20, "City: {}             P.O BOX: {}".format(parent_details.guardian_city, parent_details.guardian_pob))
+
+    # End of Guardian
+
+    # Save the PDF to the buffer
+    p.showPage()
+    p.save()
+
+    # Set the buffer's position back to the beginning
+    buffer.seek(0)
+
+    # Set the response headers for the PDF file
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="parent_details.pdf"'
 
     # Write the buffer's content to the response
     response.write(buffer.getvalue())
