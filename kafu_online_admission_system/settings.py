3c6625f63bcbc7fb760848846a26c7b3e2a4b01d
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-22 12:04:29 +0300

    custom admin app

--- kafu_online_admission_system/settings.py
+++ kafu_online_admission_system/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'admissions', # form submissions
     'submissions', # view the details submitted
     'forms_downloads', # forms download app
+    'custom_admin', # custom admin app
 ]
 
 MIDDLEWARE = [
