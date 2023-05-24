eb2002f6f41780bea7b091bd38767db59b904dce
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-05-24 11:16:08 +0300

    customize the admin site

--- kafu_online_admission_system/settings.py
+++ kafu_online_admission_system/settings.py
@@ -146,8 +146,32 @@ LOGOUT_REDIRECT_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 JAZZMIN_SETTINGS = {
-    'site_title': 'My Admin Site',
-    'site_header': 'My Admin Site',
-    'welcome_sign': 'Welcome to My Admin Site',
-    # Add more customizations as needed
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    "site_logo": "static/images/logos/KAFU_LOGO.jpg",
+
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Admin KAFU ONLINE ADMISSION",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Kafu Online Admission Admin",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Kafu Online Admission",
+
+     # Welcome text on the login screen
+    "welcome_sign": "Welcome to the admin site",
+
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": "static/images/logos/KAFU_LOGO.jpg",
+
+    # Logo to use for login form in dark themes (defaults to login_logo)
+    "login_logo_dark": 'static/images/logos/KAFU_LOGO.jpg',
+
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": None,
+
+
 }
